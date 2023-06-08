5e311fbc260f12806307e22a4c376537f9b75545
beginerbeginer <miyabi6125@gmail.com>
2023-06-08 22:20:31 +0900

    [#2] 追加：請求用アラーム用のcloudwatch

--- main.tf
+++ main.tf
@@ -11,6 +11,9 @@ provider "aws" {
   region = "us-east-1"
 }
 
+###################
+# iam
+###################
 resource "aws_iam_user" "admin" {
   name          = "Administrator"
   force_destroy = true
@@ -33,4 +36,31 @@ resource "local_file" "user_info" {
   depends_on = [aws_iam_user.admin, aws_iam_user_login_profile.admin]
   filename   = "${pathexpand("../")}/user_info.txt"
   content    = "IAM User Name: ${aws_iam_user.admin.name}\nPassword: ${aws_iam_user_login_profile.admin.password}"
+}
+
+###################
+# cloudwatch
+###################
+
+resource "aws_sns_topic" "billing" {
+  name = "Billing"
+}
+
+resource "aws_sns_topic_subscription" "user_subscription" {
+  topic_arn = aws_sns_topic.billing.arn
+  protocol  = "email"
+  endpoint  = "test@gmail.com"
+}
+
+resource "aws_cloudwatch_metric_alarm" "alarm" {
+  alarm_name          = "Billing-Alarm"
+  alarm_description   = "テストアラーム詳細"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "EstimatedCharges"
+  namespace           = "AWS/Billing"
+  period              = "21600"
+  statistic           = "Maximum"
+  threshold           = "10"
+  alarm_actions       = [aws_sns_topic.billing.arn]
 }
